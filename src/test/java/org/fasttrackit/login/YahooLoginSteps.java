4e359d69482b22d65f95911f3e63bea5eab6bfee
Cristina <bcristinabianca@yahoo.com>
2015-07-08 13:03:05 +0300

    finished yahoo login

--- src/test/java/org/fasttrackit/login/YahooLoginSteps.java
+++ src/test/java/org/fasttrackit/login/YahooLoginSteps.java
@@ -1,8 +1,6 @@
 package org.fasttrackit.login;
 
-import com.sdl.selenium.web.WebLocator;
 import com.sdl.selenium.web.utils.Utils;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -12,13 +10,18 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 /**
  * Created by Cristina on 7/7/2015.
  */
+
 public class YahooLoginSteps extends TestBase{
+    
     @Given("^I acces \"([^\"]*)\"$")
-    public void I_acces(String arg1) {
-        driver.get("https://login.yahoo.com/?.src=ym&.intl=ro&.lang=ro-RO&.done=https%3a//mail.yahoo.com");
+    public void I_acces(String arg1 ) {
+        driver.get("https://login.yahoo.com/?.src=ym&.intl=us&.lang=en-US&.done=https%3a//mail.yahoo.com");
     }
 
     @And("^I insert correct credentials$")
@@ -43,5 +46,34 @@ public class YahooLoginSteps extends TestBase{
     public void I_check_if_home_page_appears() throws Throwable {
         WebElement home = driver.findElement(By.id("Compose"));
         Assert.assertNotNull(home);
+    }
+
+    @Given("^I access \"([^\"]*)\"$")
+    public void I_access(String arg1)  {
+        driver.get("https://login.yahoo.com/?.src=ym&.intl=us&.lang=en-US&.done=https%3a//mail.yahoo.com");
+    }
+
+    @When("^I enter \"([^\"]*)\" credentials$")
+    public void I_enter_credentials(String arg1) {
+        WebElement email = driver.findElement(By.id("login-username"));
+        email.sendKeys("bcristinabianca");
+        Utils.sleep(2000);
+    }
+
+    @Then("^I expect \"([^\"]*)\" error message$")
+    public void I_expect_error_message(String expectedMessage) throws Throwable {
+        WebElement error = driver.findElement(By.id("mbr-login-error"));
+        assertThat(error.getText(), is(expectedMessage));
+    }
+
+    @When("^I enter \"([^\"]*)\"/\"([^\"]*)\" credentials$")
+    public void I_enter_credentials(String arg1, String arg2) throws Throwable {
+        WebElement email = driver.findElement(By.id("login-username"));
+        email.sendKeys(arg1);
+        //Utils.sleep(2000);
+
+        WebElement password = driver.findElement(By.id("login-passwd"));
+        password.sendKeys(arg2);
+        //Utils.sleep(2000);
     }
 }
--- src/test/resources/feature/login/yahoo.login
+++ src/test/resources/feature/login/yahoo.login
@@ -1,9 +1,28 @@
 @screen
-  Feature: Login
+Feature: Login
 
-    Scenario: Successfully login
-      Given I acces "https://login.yahoo.com/?.src=ym&.intl=ro&.lang=ro-RO&.done=https%3a//mail.yahoo.com"
-      And I insert correct credentials
-      When I click button
-      Then I check if home page appears
+  Scenario: Login with no password
+    Given I access "https://login.yahoo.com/?.src=ym&.intl=us&.lang=en-US&.done=https%3a//mail.yahoo.com"
+    When I enter "bcristinabianca" credentials
+    And I click button
+    Then I expect "Please enter your password" error message
 
+  Scenario Outline: Failed login
+    Given I acces "https://login.yahoo.com/?.src=ym&.intl=us&.lang=en-US&.done=https%3a//mail.yahoo.com"
+    When I enter "<email>"/"<password>" credentials
+    And I click button
+    Then I expect "<message>" error message
+    Examples:
+      | email           | password   | message                                                                  |
+      |                 |            | Invalid ID or password.\nPlease try again using your full Yahoo ID.      |
+      | bcristinabianca |            | Please enter your password                                               |
+      |                 | A.12!ab    | Invalid ID or password.\nPlease try again using your full Yahoo ID.      |
+      | bcristinabianca | bcdyejwu   | Invalid ID or password.\nPlease try again using your full Yahoo ID.      |
+      | ksdjwiofcj      | chdbsjakiv | This ID is not yet taken.\nAre you trying to register for a new account? |
+      | BCRISTINABIANCA | A.12!AB    | Invalid ID or password.\nPlease try again using your full Yahoo ID.      |
+
+  Scenario: Successfully login
+    Given I acces "https://login.yahoo.com/?.src=ym&.intl=us&.lang=en-US&.done=https%3a//mail.yahoo.com"
+    And I insert correct credentials
+    When I click button
+    Then I check if home page appears
